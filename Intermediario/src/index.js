f45c2733d162d43513f3846d3da4884ed67aa07d
lipe-s-m <felipe.s.m1609@gmail.com>
2025-03-11 01:37:10 -0300

    :sparkles: create project

--- Intermediario/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// funcoes multiTipos
-function funcaoMultiTipo(elemento) {
-    console.log(elemento);
-    console.log("Tipo do elemento é: " + typeof elemento);
-}
-funcaoMultiTipo(12345);
-funcaoMultiTipo("string");
